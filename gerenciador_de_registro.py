48a49c98d672c00b6e256e59ae009c5279885ca5
Yoshio Mori <yoshiomori90@gmail.com>
2015-10-02 17:00:34 -0300

    Lógica de remoção de registro

--- gerenciador_de_registro.py
+++ gerenciador_de_registro.py
@@ -11,8 +11,10 @@ lista_ligada = [None, 0, 10, None, None]
 # senão não é possível fazer a inserção
 nome = "proc1"
 tamanho = 3  # unidades
+if lista_ligada[0] is not None:  # Lançã excessão quando tenta-se fazer a inserção num registro ocupado
+    raise Exception
 if tamanho < lista_ligada[2]:
-    lista_auxiliar = [nome, 0, tamanho, None, None] # Criando novo processo de tamanho 3 unidades
+    lista_auxiliar = [nome, 0, tamanho, None, None]  # Criando novo processo de tamanho 3 unidades
     lista_auxiliar[1] = lista_ligada[1]  # Atualiza posição inicial da lista do quadro de página do processo
     lista_auxiliar[3] = lista_ligada[3]  # Atualiza lista anterior da lista do quadro de página do novo processo
     lista_auxiliar[4] = lista_ligada  # Atualiza a próxima lista da lista do quadro de página do novo processo
@@ -22,5 +24,25 @@ if tamanho < lista_ligada[2]:
     lista_ligada = lista_auxiliar
 elif tamanho == lista_ligada[2]:
     lista_ligada[0] = nome
-else:
+else:  # Lança excessão quando não há espaço suficiente
     raise Exception
+
+# Remoção de um registro
+if lista_ligada[0] is None:  # Lança excessão quando tenta-se remover registro que já está livre
+    raise Exception
+if lista_ligada[4] is not None:
+    lista_auxiliar = lista_ligada[4]
+    if lista_auxiliar[0] is None:  # Removendo o próximo registro se está livre
+        lista_auxiliar[1] = lista_ligada[1]
+        lista_auxiliar[2] += lista_ligada[2]
+        lista_auxiliar[3] = lista_ligada[3]
+        del lista_ligada[:]
+        lista_ligada = lista_auxiliar
+if lista_ligada[3] is not None:
+    lista_auxiliar = lista_ligada[3]
+    if lista_auxiliar[0] is None:  # Removendo o registro anterior se está livre
+        lista_auxiliar[2] += lista_ligada[2]
+        lista_auxiliar[4] = lista_ligada[4]
+        del lista_ligada[:]
+        lista_ligada = lista_auxiliar
+lista_ligada[0] = None
